e8a0c4ffa3f9046b9daa713c495bfc79143b04b0
eopche <tniasu@qq.com>
2024-10-05 21:41:45 +0800

    add nvim

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  -- disable trouble
+  -- { "folke/trouble.nvim",        enabled = false },
+  --
+  "williamboman/mason.nvim",
+  cmd = "Mason",
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  build = ":MasonUpdate",
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+    },
+  },
+}
