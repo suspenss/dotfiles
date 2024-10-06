8ebc8fb9e7cd69a39706fe23f10b1a3c8ae74ee6
eopche <tniasu@qq.com>
2024-10-06 19:23:46 +0800

    Add .config/nvim/lazy-lock.json.origin Update .config/yazi/init.lua

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,22 +1,23 @@
 local function module_exists(name)
-  -- 尝试通过 `pcall` 加载模块
-  local status, _ = pcall(require, name)
-  return status
+	if package.loaded[name] then
+		return true
+	else
+		local status, _ = pcall(require, name)
+		return status
+	end
 end
 
 -- 检查模块是否存在
 if not module_exists("no-status") then
-  os.execute("ya pack -i")
+	os.execute("ya pack -i")
 end
-
 
 require("full-border"):setup()
 require("no-status"):setup()
 
-
 Header:children_add(function()
-  if ya.target_family() ~= "unix" then
-    return ui.Line {}
-  end
-  return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
