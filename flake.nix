450a4a721340bd1eea440a5f86896694733b22bf
eopche <tniasu@qq.com>
2024-10-11 13:56:29 +0800

    add tools function

--- flake.nix
+++ flake.nix
@@ -7,17 +7,28 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let gen_home_conf = { name, system } : home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [ ./hosts/${name}.nix ];
-     }; in {
-      # darwin and linux
+  outputs = { nixpkgs, home-manager, ... }: let 
+    # set a symbol link
+    tools.sym = config: src: 
+      config.lib.file.mkOutOfStoreSymlink "${src}";
 
+    # flake path
+    tools.flake_path = config: 
+      "${config.home.homeDirectory}/home-manager";
+
+    gen_home_conf = { name, system } : 
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./hosts/${name}.nix ];
+        extraSpecialArgs = { 
+          inherit tools; 
+        };
+      };
+  in {
       homeConfigurations.epoche = 
         gen_home_conf { name = "epoche"; system = "x86_64-darwin"; };
      
       homeConfigurations.suspen = 
         gen_home_conf { name = "suspen"; system = "x86_64-linux"; };
-    };
+  };
 }
